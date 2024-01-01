6ec2df69fe50a2408df278ce1dcbcf479f41acc6
Jeffrey Blackburne <jblackburne@gmail.com>
2024-01-01 13:24:00 -0800

    Updated to separately track blocked and unblocked paths. But it's still wrong. :( I think I need to track nsteps remaining.

--- p17.py
+++ p17.py
@@ -4,7 +4,7 @@ from queue import PriorityQueue
 import numpy as np
 
 
-N, E, S, W, ERR = range(5)
+N, E, S, W, UNBOUND = range(5)
 DELTA = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
 
@@ -15,17 +15,12 @@ def ingest_p17(lines):
 
     return np.array(data)
 
-# This implementation is probably buggy.  It will stop following a
-# path if that path visits a square that another path has visited
-# before with a lower total cost. But that previous path might have
-# had a constraint preventing it from going a direction with lower
-# future cost, a constraint this path does not have.
 def p17a(data, max_straight=3):
     nrow, ncol = data.shape
     begin = (0, 0)
     end = (nrow - 1, ncol - 1)
 
-    min_cost = np.full_like(data, 1 << 30)
+    min_cost = np.full((nrow, ncol, 5), 1 << 30)
     tasks = PriorityQueue()
     tasks.put_nowait((0, (begin[0] + DELTA[E][0], begin[1] + DELTA[E][1]), E))
     tasks.put_nowait((0, (begin[0] + DELTA[S][0], begin[1] + DELTA[S][1]), S))
@@ -35,10 +30,22 @@ def p17a(data, max_straight=3):
             if not (0 <= irow < nrow and 0 <= icol < ncol):
                 break
             tot_cost += data[irow, icol]
-            if tot_cost < min_cost[irow, icol]:
-                min_cost[irow, icol] = tot_cost
+            if istep == max_straight - 1:
+                # Bounded in one direction. Stop if another path
+                # bounded in this direction or unbounded has an equal
+                # or lower min cost. Otherwise set this direction's
+                # min cost
+                if (min_cost[irow, icol, UNBOUND] <= tot_cost or
+                    min_cost[irow, icol, direction] <= tot_cost):
+                    break
+                min_cost[irow, icol, direction] = tot_cost
             else:
-                break
+                # Unbounded. Stop if another unbounded path has an
+                # equal or lower min cost. Otherwise set the unbounded
+                # min cost
+                if min_cost[irow, icol, UNBOUND] <= tot_cost:
+                    break
+                min_cost[irow, icol, UNBOUND] = tot_cost
 
             branches = (E, W) if direction in (N, S) else (N, S)
             for branch in branches:
@@ -50,7 +57,7 @@ def p17a(data, max_straight=3):
             icol += step[1]
 
     # Now the min cost at the end is the answer
-    return min_cost[end]
+    return np.min(min_cost[end])
 
 
 if __name__ == "__main__":
